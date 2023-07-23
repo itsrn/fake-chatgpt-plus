73fb26f622ac330fbc1f35c8fa84e390c77a70fd
Ron Nuss <shalom@itsron.space>
2023-07-23 16:05:43 +0000

    v0.4.0

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# fake-chatgpt-plus
-A chrome extension that make it look like you have chatgpt plus while on free plan
+<h1 align="center">fake-chatgpt-plus</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.4.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/itsrn/fake-chatgpt-plus/blob/main/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> A chrome extension that makes it look like you have ChatGPT Plus. Designed for free users only.
+
+## Install
+- Click on the green "code" button
+- Select download zip
+
+## Usage
+- Unzip the folder
+- Go to [Chrome's Manage Extensions page](chrome://extensions/)
+- Toggle the "developer mode" option
+- Click on "Load unpacked" button
+- Select your unzipped folder
+- Go to [ChatGPT's website](https://chat.openai.com)
+- Enjoy your fake looking ChatGPT Plus!
+
+## Author
+
+👤 **Ron Nuss**
+
+* Website: https://itsron.space
+* Github: [@itsrn](https://github.com/itsrn)
+* LinkedIn: [@itsrn](https://linkedin.com/in/itsrn)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/itsrn/fake-chatgpt-plus/issues). 
+
+## Show your support
+
+Give a ⭐️ if you likes this project and want to support me!
+
+## Version History
+
+* 0.4.0
+    * Initial Release
+
+## 📝 License
+
+Copyright © 2023 [Ron Nuss](https://github.com/itsrn).<br />
+This project is [MIT](https://github.com/itsrn/fake-chatgpt-plus/blob/main/LICENSE) licensed.
+
+***
+_This README was generated with [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.js
@@ -0,0 +1,78 @@
+function removeUpgradeButton() {
+    const upgradeToPlusButton = document.querySelectorAll(
+        ".flex.p-3.items-center.gap-3.transition-colors.duration-200.text-white",
+        ".cursor-pointer.text-sm.hover\\:bg-gray-800.rounded-md"
+    )[2]; //gets the "upgrade to plus" button element
+
+    if (upgradeToPlusButton) {
+        upgradeToPlusButton.remove(); 
+        //if exists in page remove it
+    }
+}
+
+function updateChatGptIcons() {
+    const ChatGptIcons = document.querySelectorAll(
+        ".relative.p-1.rounded-sm.h-\\[30px\\].w-\\[30px\\].text-white.flex.items-center.justify-center"
+    ); //gets an array of elements of chatgpt icons from responses
+
+    if (ChatGptIcons) { 
+        //if they are in the page
+        ChatGptIcons.forEach((element) => {
+            element.style.backgroundColor = "#715fde"; 
+            //replace the background of each one of them to the chatgpt-purple color
+        });
+    }
+}
+
+function changeButtonBackgroundColor() {
+    const button = document.querySelector(
+        ".absolute.p-1.rounded-md.right-2.disabled\\:text-gray-400",
+        ".enabled\\:bg-brand-purple.text-white.transition-colors.disabled\\:opacity-40"
+    ); //get the send button element
+
+    if (button) { 
+        //if the button exists on the page
+        const style = button.getAttribute("style"); 
+        //gets it's style
+        if (style && style.includes("background-color")) { 
+            //if the style includes a background-color
+            button.style.backgroundColor = "#715fde"; 
+            //change the background color to chatgpt-purple color
+        }
+    }
+}
+
+/*
+Currently causing weird freezes of the website so it is not included,
+until i'll find a fix for that freeze issue.
+ 
+function updateHeaderText() {
+  const headerElement = document.querySelector(
+    ".text-4xl.font-semibold.text-center.mt-6.ml-auto.mr-auto.mb-4.sm\\:mb-16.flex.gap-2.items-center.justify-center"
+  );
+ 
+  if (headerElement) {
+    headerElement.innerHTML =
+      'ChatGPT <span class="rounded-md bg-yellow-200 px-1.5 py-0.5 text-xl font-semibold uppercase text-gray-800">PLUS</span>';
+  }
+}
+*/
+
+function handleMutations() {
+    removeUpgradeButton();
+    changeButtonBackgroundColor();
+    updateChatGptIcons();
+}
+
+const observer = new MutationObserver(handleMutations);
+//creates a new observer that always looking for page changes
+//and if there are changes it will run the functions that are changing the page content
+observer.observe(document.body, { childList: true, subtree: true });
+
+if (document.readyState !== "loading") {
+    //if the page isn't loading anymore run the functions
+    handleMutations();
+} else {
+    //if the page is still loading wait to finish and then run the functions
+    document.addEventListener("DOMContentLoaded", handleMutations);
+}
